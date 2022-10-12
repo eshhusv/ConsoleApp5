42c86baa1fa6d1afb8700bf3d6be83152e47d52c
1 <1@DESKTOP-102BEE2>
2022-10-12 16:30:21 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,17 @@
+﻿//Коваленко, 22-ИСП-2/2, 6.101
+int nod;
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+while (m != n)
+{
+    if (m > n)
+    {
+        m = m - n;
+    }
+    else
+    {
+        n = n - m;
+    }
+}
+nod = n;
+Console.WriteLine("НОД: " + nod);
